0f4017d7eea2820f8a1e79c7b9f15c01a03cd9a8
ozldmezot <emils.kalnins@outlook.com>
2017-07-04 16:27:14 +0300

    Dynamic Invocation

--- src/Ozldmezot/Singleton/Singleton.php
+++ src/Ozldmezot/Singleton/Singleton.php
@@ -2,23 +2,41 @@
 
 abstract class Singleton
 {
+    protected static $classes;
+    protected static $attributes;
     protected static $instances;
 
-    public function __construct($alias, $instance)
+    public function __construct($alias, $class, $attributes = [])
     {
-        $class = get_called_class();
-        class_alias($class, $alias);
-        static::$instances[$class] = $instance;
+        $called_class = get_called_class();
+        class_alias($called_class, $alias);
+        static::$classes[$called_class]    = $class;
+        static::$attributes[$called_class] = $attributes;
     }
 
     public static function __callStatic($name, $arguments)
     {
-        $instance = static::$instances[get_called_class()];
+        $called_class = get_called_class();
+        if (!isset(static::$instances[$called_class])) {
+            $reflector = new \ReflectionClass(static::$classes[$called_class]);
+            self::$instances[$called_class] = $reflector->newInstanceArgs(static::$attributes[$called_class]);
+
+
+        }
+        $instance = static::$instances[$called_class];
         return call_user_func_array([$instance, $name], $arguments);
     }
 
     public static function instance()
     {
+        $called_class = get_called_class();
+        if (!isset(static::$instances[$called_class])) {
+            $reflector = new \ReflectionClass(static::$classes[$called_class]);
+            self::$instances[$called_class] = $reflector->newInstanceArgs(static::$attributes[$called_class]);
+
+
+        }
+        $instance = static::$instances[$called_class];
         return static::$instances[get_called_class()];
     }
 }
