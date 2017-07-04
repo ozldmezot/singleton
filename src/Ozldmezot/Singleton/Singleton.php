956cf1c88c0af6bff1f316e0fe1c3e2a3e36e2bd
ozldmezot <emils.kalnins@outlook.com>
2017-07-04 10:54:01 +0300

    Removed dynamic invocation
    Due to inaccessible properties, $instance is set to be public and the Singleton accepts an object not class and construct parameters

--- src/Ozldmezot/Singleton/Singleton.php
+++ src/Ozldmezot/Singleton/Singleton.php
@@ -2,26 +2,17 @@
 
 abstract class Singleton
 {
-    protected static $class;
-    protected static $instance;
-    protected static $arguments;
-    public function __construct($alias, $class, $arguments)
+    public static $instance;
+
+    public function __construct($alias, $instance)
     {
 
-        self::$class     = $class;
-        self::$instance  = null;
-        self::$arguments = $arguments;
+        self::$instance  = $instance;
         class_alias(__CLASS__, $alias);
     }
 
     public static function __callStatic($name, $arguments)
     {
-        if (!self::$instance) {
-            $reflector = new \ReflectionClass(self::$class);
-            self::$instance = $reflector->newInstanceArgs(self::$arguments);
-
-        }
         return call_user_func_array([self::$instance, $name], $arguments);
     }
-
 }
