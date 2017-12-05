5e772256786aa0e3b94ae2a198567d3c4a150c4d
Emīls Kalniņš <emils.kalnins@factory.digital>
2017-12-05 11:13:15 +0200

    enabled defered dependencies

--- src/Ozldmezot/Singleton/Singleton.php
+++ src/Ozldmezot/Singleton/Singleton.php
@@ -19,24 +19,36 @@ abstract class Singleton
         $called_class = get_called_class();
         if (!isset(static::$instances[$called_class])) {
             $reflector = new \ReflectionClass(static::$classes[$called_class]);
-            self::$instances[$called_class] = $reflector->newInstanceArgs(static::$attributes[$called_class]);
-
-
+            $attributes = static::resolve(static::$attributes[$called_class]);
+            self::$instances[$called_class] = $reflector->newInstanceArgs($attributes);
         }
         $instance = static::$instances[$called_class];
+
         return call_user_func_array([$instance, $name], $arguments);
     }
 
     public static function instance()
     {
         $called_class = get_called_class();
+
         if (!isset(static::$instances[$called_class])) {
-            $reflector = new \ReflectionClass(static::$classes[$called_class]);
-            self::$instances[$called_class] = $reflector->newInstanceArgs(static::$attributes[$called_class]);
-
-
+            $reflector  = new \ReflectionClass(static::$classes[$called_class]);
+            $attributes = static::resolve(static::$attributes[$called_class]);
+            self::$instances[$called_class] = $reflector->newInstanceArgs($attributes);
         }
         $instance = static::$instances[$called_class];
+
         return static::$instances[get_called_class()];
+    }
+
+    protected static function resolve($attributes)
+    {
+        foreach ($attributes as $key => $attribute) {
+            if ($attribute instanceof Singleton) {
+                $attributes[$key] = $attribute::instance();
+            }
+        }
+
+        return $attributes;
     }
 }
